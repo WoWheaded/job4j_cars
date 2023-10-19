f5c47b9c5b27224a25c0f235be44ff430570a792
Axiom <d-343@yandex.ru>
2023-10-19 20:27:53 +0300

    3. Доменная модель проекта "АвтоМаг" [#504897]

---
+++ db/scripts/002_ddl_create_auto_post_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE auto_post
+(
+    id       SERIAL PRIMARY KEY,
+    description    VARCHAR NOT NULL ,
+    created        TIMESTAMP NOT NULL,
+    auto_user_id   INT REFERENCES auto_user(id)
+);
