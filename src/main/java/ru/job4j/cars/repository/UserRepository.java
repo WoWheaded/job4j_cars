fd985641923b873b2feaa975eb7971aa60898260
Axiom <d-343@yandex.ru>
2023-10-21 00:08:17 +0300

    2. HQL [#6874]

---
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -0,0 +1,127 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import ru.job4j.cars.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserRepository {
+    private final SessionFactory sf;
+
+    public User create(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return user;
+    }
+
+    public void update(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "UPDATE User SET login = :fLogin, password = :fPassword WHERE id = :fId")
+                    .setParameter("fLogin", user.getLogin())
+                    .setParameter("fPassword", user.getPassword())
+                    .setParameter("fId", user.getId());
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void delete(int userId) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "DELETE User WHERE id = :fId")
+                    .setParameter("fId", userId);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+    }
+
+    public List<User> findAllOrderById() {
+        List<User> list = List.of();
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            list = session.createQuery(
+                    "FROM User ORDER BY id", User.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return list;
+    }
+
+    public Optional<User> findById(int userId) {
+        Optional<User> foundedUser = Optional.empty();
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            foundedUser = session.createQuery(
+                            "FROM User as u WHERE u.id = :fId", User.class)
+                    .setParameter("fId", userId).uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return foundedUser;
+    }
+
+    public List<User> findByLikeLogin(String key) {
+        Session session = sf.openSession();
+        List<User> rsl = List.of();
+        try {
+            session.beginTransaction();
+            rsl = session.createQuery(
+                            "FROM User as u WHERE u.login LIKE :fKey", User.class)
+                    .setParameter("fKey", "%" + key + "%").list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+
+    public Optional<User> findByLogin(String login) {
+        Session session = sf.openSession();
+        Optional<User> rsl = Optional.empty();
+        try {
+            session.beginTransaction();
+            rsl = session.createQuery(
+                            "FROM User as u WHERE u.login LIKE :fLogin", User.class)
+                    .setParameter("fLogin", login).uniqueResultOptional();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+}
